e3ff5617c54dc24b3fc14030fa5f863a7556e6a3
Koki Takahashi <hakatasiloving@gmail.com>
2018-07-13 18:41:15 +0900

    Beautify

--- genpic.py
+++ genpic.py
@@ -1,15 +1,13 @@
 import matplotlib
 from matplotlib import pyplot as plt
 
-
 plt.figure(figsize=matplotlib.figure.figaspect(1))
 
-for i in range(0,4):
-	plt.plot([i,i],[0,3],color='black')
-	plt.plot([0,3],[i,i],color='black')
+for i in range(0, 4):
+	plt.plot([i, i], [0, 3], color='black')
+	plt.plot([0, 3], [i, i], color='black')
 
-plt.tick_params(labelbottom=False,labelleft=False,color='white')
-plt.xlim(0,3)
-plt.ylim(0,3)
+plt.tick_params(labelbottom=False, labelleft=False, color='white')
+plt.xlim(0, 3)
+plt.ylim(0, 3)
 plt.show()
-
